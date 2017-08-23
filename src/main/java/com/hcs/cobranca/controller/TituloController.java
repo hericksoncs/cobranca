6f9edce89c1f62d922d9b9a5dab6e8bd660691c2
hericksoncs <hericksoncs@gmail.com>
2017-08-23 16:40:37 -0300

    HCS-23/08/2017

--- src/main/java/com/hcs/cobranca/controller/TituloController.java
+++ src/main/java/com/hcs/cobranca/controller/TituloController.java
@@ -1,12 +1,20 @@
 package com.hcs.cobranca.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hcs.cobranca.model.StatusTitulo;
 import com.hcs.cobranca.model.Titulo;
 import com.hcs.cobranca.repository.titulos;
+
 
 @Controller
 @RequestMapping("/titulos")
@@ -16,8 +24,10 @@ public class TituloController {
 	private titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -25,8 +35,17 @@ public class TituloController {
 		titulos.save(titulo);
 		
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem","Titulo salco com sucesso!");
+		mv.addObject("mensagem","Titulo salvo com sucesso!");
 		return mv;
 	}
 	
+	@RequestMapping
+	public String pesquisar() {
+		return "PesquisaTitulos";
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+	}
 }
