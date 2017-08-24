ff6ebd404cb6a5fa631aeb5fc35a344e7bf71296
hericksoncs <hericksoncs@gmail.com>
2017-08-24 16:55:13 -0300

    HCS-24/08/2017

--- src/main/java/com/hcs/cobranca/model/Titulo.java
+++ src/main/java/com/hcs/cobranca/model/Titulo.java
@@ -63,6 +63,11 @@ public class Titulo {
 	public void setStatus(StatusTitulo status) {
 		this.status = status;
 	}
+	
+	public boolean isPendente() {
+		return StatusTitulo.PENDENTE.equals(status);
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
