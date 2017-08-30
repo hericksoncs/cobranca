e00503cb58fd91cc8564de9b23949249db193301
hericksoncs <hericksoncs@gmail.com>
2017-08-30 15:11:47 -0300

    HCS-30/08/2017

--- src/main/java/com/hcs/cobranca/controller/TituloController.java
+++ src/main/java/com/hcs/cobranca/controller/TituloController.java
@@ -14,10 +14,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import com.hcs.cobranca.model.StatusTitulo;
 import com.hcs.cobranca.model.Titulo;
 import com.hcs.cobranca.repository.titulos;
+import com.hcs.cobranca.service.CadastroTituloService;
 
 
 @Controller
@@ -28,6 +30,9 @@ public class TituloController {
 	
 	@Autowired
 	private titulos titulos;
+	
+	@Autowired
+	private CadastroTituloService cadastroTituloServce;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
@@ -43,10 +48,14 @@ public class TituloController {
 			return CADASTRO_VIEW;
 		}
 		
-		titulos.save(titulo);
-		
-		attributes.addFlashAttribute("mensagem","Titulo salvo com sucesso!");
-		return "redirect:/titulos/novo";
+		try {
+			cadastroTituloServce.salvar(titulo);
+			attributes.addFlashAttribute("mensagem","Titulo salvo com sucesso!");
+			return "redirect:/titulos/novo";
+		}catch(DataIntegrityViolationException e) {
+			errors.rejectValue("dataVencimento", null, e.getMessage());
+			return CADASTRO_VIEW;
+		}
 	}
 	
 	@RequestMapping
@@ -58,14 +67,20 @@ public class TituloController {
 	}
 	
 	@RequestMapping("{codigo}")
-	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) { //Long codigo) {
-		// Titulo titulo=titulos.findOne(codigo); --> Substituido por @PathVariable("codigo")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) { 
 		
 		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(titulo);
 		return mv;
 	}
 	
+	@RequestMapping(value="{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
+		cadastroTituloServce.excluir(codigo);
+		
+		attributes.addFlashAttribute("mensagem","Titulo excluído com sucesso!");
+		return "redirect:/titulos";
+	}
 	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo(){
---
+++ src/main/java/com/hcs/cobranca/service/CadastroTituloService.java
@@ -0,0 +1,27 @@
+package com.hcs.cobranca.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.hcs.cobranca.model.Titulo;
+import com.hcs.cobranca.repository.titulos;
+
+@Service
+public class CadastroTituloService {
+
+	@Autowired
+	private titulos titulos;
+	
+	public void salvar(Titulo titulo) {
+		try {
+			titulos.save(titulo);
+		} catch(DataIntegrityViolationException e) {
+			throw new IllegalArgumentException("Formato de data inválido");
+		}
+	}
+
+	public void excluir(Long codigo) {
+		titulos.delete(codigo);
+	}
+}
